75676f76acc1c1380b1f1f3eee5540e056fe7e50
Rafael Vescovi <ravescovi@gmail.com>
2024-06-10 14:09:22 -0500

    test

---
+++ src/qsui/test2.py
@@ -0,0 +1,23 @@
+""""""
+
+import sys
+
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+
+
+def main():
+    """"""
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+    win.setWindowTitle("My first window!")
+
+    label = QLabel(win)
+    label.setText("my first label")
+    label.move(50, 50)
+
+    win.show()
+    sys.exit(app.exec_())
+
+
+main()
